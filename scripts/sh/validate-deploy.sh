9f386b5a9452e358764cfd15a91f2bf4d7b9c97d
Konstantin Sitkin <konstantin.sitkin@icloud.com>
2022-08-10 14:49:47 +0300

    added mocks, test coverage, fixed error response order confirmation

--- scripts/sh/validate-deploy.sh
+++ scripts/sh/validate-deploy.sh
@@ -1,6 +1,6 @@
 sfdx force:source:deploy \
 -p=force-app \
 --testlevel=RunSpecifiedTests \
---runtests=KPN_OrderItemSelectorTest,KPN_OrderProductCtrlTest,KPN_TestDataFactory,KPN_OrderSelectProductsCtrlTest,KPN_OrderViewOrderItemsCtrlTest,KPN_OrderSelectorTest \
+--runtests=KPN_OrderItemSelectorTest,KPN_OrderProductCtrlTest,KPN_TestDataFactory,KPN_OrderSelectProductsCtrlTest,KPN_OrderViewOrderItemsCtrlTest,KPN_OrderSelectorTest,KPN_OrderConfirmationCtrlTest,KPN_ExternalSystemPostOrderMock \
 --checkonly \
 -u=$1
